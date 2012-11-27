0a854f3be28510f2c5dc1d56cde3c585c4a476d4
rawhay <aes_cbc_256@yahoo.com>
2012-11-27 22:53:47 +0500

    Added files related to pbkdf2

---
+++ cryptx2/src/cryptX2/pbkdf2.c
@@ -0,0 +1,8 @@
+/*
+ * pbkdf2.c
+ *
+ * Created: 11/27/2012 9:21:53 PM
+ *  Author: mwaqas
+ */ 
+
+
---
+++ cryptx2/src/cryptX2/pbkdf2.h
@@ -0,0 +1,16 @@
+/*
+ * pbkdf2.h
+ *
+ * Created: 11/27/2012 9:22:05 PM
+ *  Author: mwaqas
+ */ 
+
+
+#ifndef PBKDF2_H_
+#define PBKDF2_H_
+
+
+
+
+
+#endif /* PBKDF2_H_ */
